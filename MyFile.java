1f97549ed8a65664a6d5a49c664e19dfa486ac78
lrelango <lrelango@gmail.com>
2021-06-09 19:21:12 +0530

    Second Commit

--- MyFile.java
+++ MyFile.java
@@ -2,5 +2,6 @@ public class MyFile{
     public static void main(String args[]){
         System.out.println("This is my first project file");
         System.out.println("Onemore new line added to print");
+        System.out.println("Onemore new line added to print1");
     }
 }
